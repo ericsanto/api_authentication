1a2f5a2beffa13ff5779018c4d0b624933028af4
Éric Santos <ericjesus403@gmail.com>
2025-04-30 11:28:13 -0300

    chore: database configuration

---
+++ config/database/database.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/ericsanto/api_authentication/config"
+	"github.com/ericsanto/api_authentication/internal/user"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDatabase() error {
+
+	dsn := "host=db user=user password=user dbname=user port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	if err := db.AutoMigrate(&user.UserModel{}); err != nil {
+		return fmt.Errorf("erro ao migrar tabelas para o banco de dados %s", err.Error())
+	}
+
+	config.DB = db
+
+	return nil
+
+}
