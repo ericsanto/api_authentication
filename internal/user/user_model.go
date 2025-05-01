1ad80144a0afe06e90f667996e1b28c20f830a86
Éric Santos <ericjesus403@gmail.com>
2025-05-01 18:17:30 -0300

    feat: relate user entity with farm

--- internal/user/user_model.go
+++ internal/user/user_model.go
@@ -1,8 +1,11 @@
 package user
 
+import "github.com/ericsanto/api_authentication/internal/farm"
+
 type UserModel struct {
-	ID       uint   `gorm:"primaryKey;autoIncrement"`
-	Name     string `gorm:"size:100"`
-	Email    string `gorm:"size:255;unique"`
-	Password string `gorm:"size:255"`
+	ID       uint        `gorm:"primaryKey;autoIncrement"`
+	Name     string      `gorm:"size:100"`
+	Email    string      `gorm:"size:255;unique"`
+	Password string      `gorm:"size:255"`
+	Farm     []farm.Farm `gorm:"foreignKey:UserID"`
 }
